1c126b90dbb14ddce49ee2456c51a4ee5bbf1dfc
Dann Stockton <dannstockton@gmail.com>
2021-01-16 22:55:13 -0700

    Add login request

---
+++ client/src/LoginRequest.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+import { Redirect, useParams } from "react-router-dom";
+
+export default function LoginRequest() {
+  let { token } = useParams();
+  const [location, setLocation] = useState(null);
+  useEffect(() => {
+    async function loginRequest() {
+      await fetch("/session", {
+        method: "POST",
+        body: JSON.stringify({ token }),
+      })
+        .then((res) => res.json())
+        .then((j) => {
+          setLocation(j.location);
+        });
+    }
+
+    if (token !== undefined) {
+      loginRequest();
+    }
+  }, []);
+  return location && <Redirect to={location} />;
+}
