b23a3b498a8416e3f3c4c350253f37e0fee10e8b
Dann Stockton <dannstockton@gmail.com>
2021-01-16 21:11:15 -0700

    Add RNG

---
+++ server/util/random.go
@@ -0,0 +1,31 @@
+package util
+
+import (
+	"crypto/rand"
+	"math/big"
+)
+
+// stolen from https://gist.github.com/denisbrodbeck/635a644089868a51eccd6ae22b2eb800
+
+// GenerateRandomASCIIString returns a securely generated random ASCII string.
+// It reads random numbers from crypto/rand and searches for printable characters.
+// It will return an error if the system's secure random number generator fails to
+// function correctly, in which case the caller must not continue.
+func GenerateRandomASCIIString(length int) (string, error) {
+	result := ""
+	for {
+		if len(result) >= length {
+			return result, nil
+		}
+		num, err := rand.Int(rand.Reader, big.NewInt(int64(127)))
+		if err != nil {
+			return "", err
+		}
+		n := num.Int64()
+		// Make sure that the number/byte/letter is inside
+		// the range of printable ASCII characters (excluding space and DEL)
+		if n > 32 && n < 127 {
+			result += string(n)
+		}
+	}
+}
