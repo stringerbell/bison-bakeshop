d2b89627475a81a29610e34d8dbebcfc88db2a34
Dann Stockton <dannstockton@gmail.com>
2021-01-16 22:57:35 -0700

    Add routes

--- server/server.go
+++ server/server.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	_ "bison-bakeshop/memory"
+	session2 "bison-bakeshop/session"
+	"bison-bakeshop/util"
 	"bytes"
 	"context"
 	"crypto/subtle"
+	"encoding/base64"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -30,6 +34,17 @@ import (
 )
 
 var conn *pgx.Conn
+var mgr *session2.Manager
+
+// initialize in init() function
+func init() {
+	globalSessions, err := session2.NewManager("memory", "sess", time.Hour*24*30)
+	if err != nil {
+		log.Fatal("error session2.NewManager", err)
+	}
+	mgr = globalSessions
+	go mgr.GC()
+}
 
 func main() {
 	err := godotenv.Load()
@@ -53,14 +68,210 @@ func main() {
 	http.HandleFunc("/webhook", handleWebhook)
 	http.HandleFunc("/incoming-email", handleIncomingEmail)
 	http.HandleFunc("/account", handleCreateAccount)
+	http.HandleFunc("/login", handleLogin)
+	http.HandleFunc("/logout", handleLogout)
+	http.HandleFunc("/session", handleCreateSession)
+	http.HandleFunc("/me", handleSessionRead)
 
 	log.Println("server running at 0.0.0.0:4242")
 	http.ListenAndServe("0.0.0.0:4242", nil)
 }
 
-type accountCreateBody struct {
-	SessionID string `json:"sessionID"`
-	Email     string `json:"email"`
+func handleLogout(w http.ResponseWriter, r *http.Request) {
+	mgr.SessionDestroy(w, r)
+	writeJSON(w, nil)
+}
+
+type identity struct {
+	LoggedIn   bool   `json:"logged_in,omitempty"`
+	CustomerID string `json:"customer_id,omitempty"`
+}
+
+const sessLoggedIn = "logged_in"
+const sessCustomerID = "customer_id"
+const sessCustomerEmail = "customer_email"
+
+func handleSessionRead(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	sess := mgr.SessionStart(w, r)
+	loggedIn, _ := sess.Get(sessLoggedIn).(bool)
+	customerID, _ := sess.Get(sessCustomerID).(string)
+	writeJSON(w, identity{
+		LoggedIn:   loggedIn,
+		CustomerID: customerID,
+	})
+}
+
+type sessionBody struct {
+	Token string `json:"token"`
+}
+type sessionResponse struct {
+	Location string `json:"location"`
+}
+
+func handleCreateSession(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	j := sessionBody{}
+	json.NewDecoder(r.Body).Decode(&j)
+	if j.Token == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	bs, err := base64.URLEncoding.DecodeString(j.Token)
+	if err != nil {
+		writeJSON(w, sessionResponse{
+			Location: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+		})
+		return
+	}
+	sess := mgr.SessionStart(w, r)
+
+	if loggedIn, _ := sess.Get(sessLoggedIn).(bool); loggedIn {
+		writeJSON(w, sessionResponse{
+			Location: "/",
+		})
+		return
+	}
+
+	var loginTokenID *int64
+	rows, err := conn.Query(context.Background(), `
+  select login_tokens.id, accounts.customer, accounts.email
+  from bakery.login_tokens
+           join bakery.accounts on login_tokens.user_id = accounts.id
+  where token = $1
+    and login_tokens.created_at > now() - interval '10 minutes'
+    and used_at is null
+  `, string(bs))
+	if err != nil {
+		fmt.Printf("conn.QueryRow: %v", err)
+		return
+	}
+
+	var customerID *string
+	var customerEmail *string
+	for rows.Next() {
+		rows.Scan(&loginTokenID, &customerID, &customerEmail)
+	}
+	if loginTokenID == nil || *loginTokenID == 0 {
+		writeJSON(w, sessionResponse{
+			Location: "/",
+		})
+		return
+	}
+
+	_, err = conn.Exec(context.Background(), `
+update bakery.login_tokens set used_at = now() where login_tokens.id = $1
+`, loginTokenID)
+	if err != nil {
+		fmt.Printf("conn.Exec: %v", err)
+		writeJSON(w, sessionResponse{
+			Location: "/login",
+		})
+		return
+	}
+	sess.Set(sessLoggedIn, true)
+	sess.Set(sessCustomerID, *customerID)
+	sess.Set(sessCustomerEmail, *customerEmail)
+	writeJSON(w, sessionResponse{
+		Location: "/",
+	})
+}
+
+type postBody struct {
+	SessionID string `json:"sessionID,omitempty"`
+	Email     string `json:"email,omitempty"`
+}
+
+func handleLogin(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	j := postBody{}
+	json.NewDecoder(r.Body).Decode(&j)
+	if j.Email == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	go sendLoginLink(j.Email)
+	writeJSON(w, j)
+}
+
+func sendLoginLink(email string) {
+	var emailID *int64
+	err := conn.QueryRow(context.Background(), "select id from bakery.accounts where email = $1", email).Scan(&emailID)
+	if err != nil {
+		fmt.Printf("conn.QueryRow: %v, email: %v\n", err, email)
+		return
+	}
+	if emailID == nil {
+		fmt.Printf("no email found to send to. Requested: %v\n", email)
+		return
+	}
+	var unusedLogins *int
+	conn.QueryRow(context.Background(), `
+  select count(*)
+  from bakery.login_tokens
+  where user_id = $1
+    and created_at > now() - interval '10 minutes'
+    and used_at is null
+`, emailID).Scan(&unusedLogins)
+	if unusedLogins != nil && *unusedLogins != 0 {
+		fmt.Printf("user (%s) who has unused login token is requesting a login\n", email)
+		return
+	}
+
+	// we'll send a login link to their email
+	m := mail.NewV3Mail()
+	from := mail.NewEmail("Bison Bakeshop", os.Getenv("FROM_EMAIL"))
+	m.SetReplyTo(mail.NewEmail("Dann", os.Getenv("REPLY_TO")))
+	tokenLen, _ := strconv.Atoi(os.Getenv("TOKEN_LENGTH"))
+	loginLink, err := util.GenerateRandomASCIIString(tokenLen)
+	if err != nil {
+		fmt.Printf("util.GenerateRandomASCIIString: %v", err)
+		return
+	}
+	_, err = conn.Exec(context.Background(), "insert into bakery.login_tokens (token, user_id) values ($1,$2)", loginLink, emailID)
+	if err != nil {
+		fmt.Printf("conn.Exec: %v", err)
+		return
+	}
+	content := mail.NewContent("text/plain", fmt.Sprintf(`
+Hello!
+
+A login has been requested for bison bakeshop. Please click the link below to login.
+
+https://bisonbakeshop.com/login/%s
+
+If you didn't request this, you can safely ignore this, and, I'd appreciate a heads up, so I can look into it.
+
+Thank you!
+Dann
+`, base64.URLEncoding.EncodeToString([]byte(loginLink))))
+
+	m.SetFrom(from)
+	m.AddContent(content)
+	personalization := mail.NewPersonalization()
+	personalization.AddTos(mail.NewEmail("", email))
+	personalization.Subject = "Your login request"
+	m.AddPersonalizations(personalization)
+	client := sendgrid.NewSendClient(os.Getenv("SENDGRID_API_KEY"))
+	if os.Getenv("NAME") == "prod" {
+		re, err := client.Send(m)
+		if err != nil {
+			log.Printf("client.Send err: %v", err)
+			return
+		}
+		log.Printf(strconv.Itoa(re.StatusCode))
+	} else {
+		fmt.Printf("would send email: %v", m.Content[0])
+	}
 }
 
 func handleCreateAccount(w http.ResponseWriter, r *http.Request) {
@@ -68,7 +279,7 @@ func handleCreateAccount(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
-	j := accountCreateBody{}
+	j := postBody{}
 	json.NewDecoder(r.Body).Decode(&j)
 	if j.SessionID == "" || j.Email == "" {
 		w.WriteHeader(http.StatusBadRequest)
@@ -76,7 +287,7 @@ func handleCreateAccount(w http.ResponseWriter, r *http.Request) {
 	}
 	s, err := session.Get(j.SessionID, nil)
 	if err != nil {
-		fmt.Printf("session.Get: %w", err)
+		fmt.Printf("session.Get: %v", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
@@ -95,15 +306,11 @@ func handleCreateAccount(w http.ResponseWriter, r *http.Request) {
 		var pgErr *pgconn.PgError
 		if errors.As(err, &pgErr) {
 			if pgErr.Code == "23505" {
-				writeJSONError(w, struct {
-					Error string `json:"error"`
-				}{Error: "would you like to login?"}, http.StatusConflict)
+				writeJSONErrorMessage(w, "would you like to login?", http.StatusConflict)
 				return
 			}
 		}
-		writeJSONError(w, struct {
-			Error string `json:"error"`
-		}{Error: "unable to save your account"}, http.StatusInternalServerError)
+		writeJSONErrorMessage(w, "unable to save your account", http.StatusInternalServerError)
 		return
 	}
 	writeJSON(w, nil)
@@ -230,8 +437,11 @@ type preSale struct {
 }
 
 func handlePreSales(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		rs, err := conn.Query(context.Background(), `
+	if r.Method != http.MethodGet {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+	rs, err := conn.Query(context.Background(), `
     select presales.id,
            coalesce(sum(qty), 0) as sold,
            presales.total_qty,
@@ -241,26 +451,25 @@ func handlePreSales(w http.ResponseWriter, r *http.Request) {
     where presales.deleted = false
     group by bakery.orders.presale_id, presales.id
     order by presale_id`,
-		)
-		if err != nil {
-			log.Fatal("conn.Query", err)
-		}
-		pss := []preSale{}
-		for rs.Next() {
-			var ps preSale
-			err := rs.Scan(&ps.ID, &ps.Sold, &ps.Total, &ps.Date)
-			if err != nil {
-				log.Fatal("rs.Scan", err)
-			}
-			ps.Available = ps.Total - ps.Sold // ugh
-			pss = append(pss, ps)
-		}
-		if rs.Err() != nil {
-			log.Fatal("rs.Err", rs.Err())
-		}
-		writeJSON(w, pss)
-		return
+	)
+	if err != nil {
+		log.Fatal("conn.Query", err)
 	}
+	pss := []preSale{}
+	for rs.Next() {
+		var ps preSale
+		err := rs.Scan(&ps.ID, &ps.Sold, &ps.Total, &ps.Date)
+		if err != nil {
+			log.Fatal("rs.Scan", err)
+		}
+		ps.Available = ps.Total - ps.Sold // ugh
+		pss = append(pss, ps)
+	}
+	if rs.Err() != nil {
+		log.Fatal("rs.Err", rs.Err())
+	}
+	writeJSON(w, pss)
+	return
 }
 
 func handleConfig(w http.ResponseWriter, r *http.Request) {
@@ -319,13 +528,35 @@ type checkoutSessionCreateReq struct {
 }
 
 func handleCreateCheckoutSession(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	sess := mgr.SessionStart(w, r)
 	req := checkoutSessionCreateReq{}
 	json.NewDecoder(r.Body).Decode(&req)
+	customerID, _ := sess.Get(sessCustomerID).(string)
+	customerEmail, _ := sess.Get(sessCustomerEmail).(string)
+	if customerID != "" {
+		err := createReservation(req, customerEmail, customerID)
+		if err != nil {
+			writeJSONErrorMessage(w, fmt.Sprintf("can't create pre-order: %v", err), http.StatusInternalServerError)
+		} else {
+			go sendPreorderEmail(preorderEmail{
+				qty:           int(req.Quantity),
+				date:          req.Date.Format("2006-01-02"),
+				price:         float64(priceForQuantity(req.Quantity)),
+				customerEmail: customerEmail,
+			})
+			writeJSON(w, "👍🏻")
+		}
+		return
+	}
 	domainURL := os.Getenv("DOMAIN")
 	cParams := &stripe.CustomerParams{}
 	c, err := customer.New(cParams)
 	if err != nil {
-		writeJSONError(w, fmt.Errorf("can't create customer: %w", err), http.StatusInternalServerError)
+		writeJSONErrorMessage(w, fmt.Sprintf("can't create customer: %v", err), http.StatusInternalServerError)
 		return
 	}
 
@@ -368,6 +599,74 @@ func handleCreateCheckoutSession(w http.ResponseWriter, r *http.Request) {
 		SessionID: s.ID,
 	})
 }
+
+type preorderEmail struct {
+	qty           int
+	date          string // 2020-01-01
+	price         float64
+	customerEmail string
+}
+
+func sendPreorderEmail(e preorderEmail) {
+	layout := "Mon, Jan _2, 2006"
+	date, _ := time.Parse("2006-01-02", e.date)
+	m := mail.NewV3Mail()
+	from := mail.NewEmail("Bison Bakeshop", os.Getenv("FROM_EMAIL"))
+	m.SetReplyTo(mail.NewEmail("Dann", os.Getenv("REPLY_TO")))
+	content := mail.NewContent("text/plain", fmt.Sprintf(`
+Thanks for your pre-order!
+
+Details:
+  - Pickup date: %v (Rolls are out of the oven at 8am)
+  - Quantity: %v
+  - Total Price: $%.2f
+
+My address is:
+%s
+
+If you have any questions, respond to this email, or feel free to call/text me: %s
+
+Thank you!
+Dann
+`, date.Format(layout),
+		e.qty,
+		e.price/100,
+		os.Getenv("PICKUP_ADDRESS"),
+		os.Getenv("SUPPORT_PHONE"),
+	),
+	)
+	m.SetFrom(from)
+	m.AddContent(content)
+	personalization := mail.NewPersonalization()
+	personalization.AddTos(mail.NewEmail("", e.customerEmail))
+	personalization.AddBCCs(mail.NewEmail("", os.Getenv("BCC_EMAIL")))
+	personalization.Subject = "Your cinnamon roll pre-order for " + date.Format(layout)
+	m.AddPersonalizations(personalization)
+	client := sendgrid.NewSendClient(os.Getenv("SENDGRID_API_KEY"))
+	if os.Getenv("NAME") == "prod" {
+		re, err := client.Send(m)
+		if err != nil {
+			log.Printf("client.Send err: %v", err)
+			return
+		}
+		log.Printf(strconv.Itoa(re.StatusCode))
+	} else {
+		fmt.Printf("would send email: %v", m.Content[0])
+	}
+}
+
+func createReservation(req checkoutSessionCreateReq, customerEmail, customerID string) error {
+	t, err := conn.Exec(context.Background(),
+		`
+      insert into bakery.orders (email, presale_id, qty, price, customer_id, session_id)
+      values ($1, $2, $3, $4, $5, $6)
+    `,
+		customerEmail, req.ID, req.Quantity, strconv.FormatInt(priceForQuantity(req.Quantity), 10), customerID, nil,
+	)
+	fmt.Println(t)
+	return err
+}
+
 func priceForQuantity(q int64) int64 {
 	// Fetch a price, use its unit amount and currency
 	p, _ := price.Get(
@@ -386,7 +685,7 @@ func priceForQuantity(q int64) int64 {
 }
 
 func handleWebhook(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
+	if r.Method != http.MethodPost {
 		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
 		return
 	}
@@ -409,65 +708,21 @@ func handleWebhook(w http.ResponseWriter, r *http.Request) {
 		var sesh stripe.CheckoutSession
 		_ = json.Unmarshal(event.Data.Raw, &sesh)
 		go addPreorder(sesh)
-		go sendEmail(sesh)
-	}
-	writeJSON(w, event)
-}
-
-func sendEmail(event stripe.CheckoutSession) {
-	if event.Customer == nil {
-		log.Printf("nil customer %v", event.ID)
-		return
-	}
-	c, err := customer.Get(event.Customer.ID, nil)
-	if err != nil {
-		log.Printf("customer.Get %v", err)
-		return
-	}
-	qty := event.Metadata["qty"]
-	d := event.Metadata["requested_date"]
-	layout := "Mon, Jan _2, 2006"
-	date, _ := time.Parse("2006-01-02", d)
-	p, _ := strconv.ParseFloat(event.Metadata["price"], 64)
-
-	m := mail.NewV3Mail()
-	from := mail.NewEmail("Bison Bakeshop", os.Getenv("FROM_EMAIL"))
-	m.SetReplyTo(mail.NewEmail("Dann", os.Getenv("REPLY_TO")))
-	content := mail.NewContent("text/plain", fmt.Sprintf(`
-Thanks for your pre-order!
-
-Details:
-  - Pickup date: %v (Rolls are out of the oven at 8am)
-  - Quantity: %v
-  - Total Price: $%.2f
-
-My address is:
-%s
-
-If you have any questions, respond to this email, or feel free to call/text me: %s
-
-Thank you!
-Dann
-`, date.Format(layout), qty, p/100, os.Getenv("PICKUP_ADDRESS"), os.Getenv("SUPPORT_PHONE")),
-	)
-	m.SetFrom(from)
-	m.AddContent(content)
-	personalization := mail.NewPersonalization()
-	personalization.AddTos(mail.NewEmail("", c.Email))
-	personalization.AddBCCs(mail.NewEmail("", os.Getenv("BCC_EMAIL")))
-	personalization.Subject = "Your cinnamon roll pre-order for " + date.Format(layout)
-	m.AddPersonalizations(personalization)
-	client := sendgrid.NewSendClient(os.Getenv("SENDGRID_API_KEY"))
-	if os.Getenv("NAME") == "prod" {
-		re, err := client.Send(m)
+		c, err := customer.Get(sesh.Customer.ID, nil)
 		if err != nil {
-			log.Printf("client.Send err: %v", err)
+			log.Printf("customer.Get %v", err)
 			return
 		}
-		log.Printf(strconv.Itoa(re.StatusCode))
-	} else {
-		fmt.Printf("would send: %v", m)
+		qty, _ := strconv.Atoi(sesh.Metadata["qty"])
+		totalPrice, _ := strconv.ParseFloat(sesh.Metadata["price"], 64)
+		go sendPreorderEmail(preorderEmail{
+			qty:           qty,
+			date:          sesh.Metadata["requested_date"],
+			price:         totalPrice,
+			customerEmail: c.Email,
+		})
 	}
+	writeJSON(w, event)
 }
 
 func addPreorder(event stripe.CheckoutSession) {
